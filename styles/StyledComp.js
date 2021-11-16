317412bf44798a6c2d0fe998616f1f33c6f78ae6
Emmanuel251094 <69593511+Emmanuel251094@users.noreply.github.com>
2021-11-16 13:18:09 -0600

    Add files via upload

---
+++ styles/StyledComp.js
@@ -0,0 +1,52 @@
+import styled from 'styled-components/native';
+
+export const StyledView = styled.View`
+    background-color: white;
+    flex: 1;
+    justify-content: center;
+    align-items: left;
+    `;
+
+export const StyledText = styled.Text`
+    color: black;
+    font-size: 24px;
+    `;
+
+
+// Counter Styles
+
+export const StyledNotValid = styled.Text`
+color: red;
+`;
+
+export const StyledValid = styled.Text`
+color: blue;
+`;
+
+export const StyledNormalText = styled.Text`
+    font-size: 35px;
+    color: black;
+`;
+ 
+export const StyledButtonText = styled.Text`
+font-size: 40px;
+color: white;
+`;
+
+export const StyledBoldText = styled.Text`
+font-weight: bold;
+`;
+
+export const StyledButtonStyles = styled.Text`
+border-radius: 25px;
+padding: 5px;
+margin: 10px;
+background-color: blue;
+width: 300px;
+text-align:justify ;
+`;
+
+export const StyledTextP = styled.Text`
+font-size: 45px;
+`;
+  
